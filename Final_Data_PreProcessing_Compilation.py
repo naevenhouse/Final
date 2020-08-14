4f15c4fdf413a734444b5b13bc2fdc10d56dab6b
naevenhouse <naevenho@mtu.edu>
2020-08-13 22:10:33 -0400

    Split preprocessing into comlilation and scaping. scraping designed to run for extended periods of time within the goodreads guidelines.

--- Final_Data_PreProcessing_Compilation.py
+++ Final_Data_PreProcessing_Compilation.py
@@ -8,20 +8,13 @@ Nathaniel Evenhouse
 import os
 import time
 import pandas as pd
-from goodreads import client
-import numpy as np
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-import matplotlib.pyplot as plt
-
 dataFolder = 'Data'
-
-#initialize client for goodreads
-gc = client.GoodreadsClient('fm1h6CpTTcPHEgwUAvOD1w', 'l62WO2kz3lmJifV7VkGZY4SAcWzkjtUw3Unj7zjE')
-gc.authenticate('fm1h6CpTTcPHEgwUAvOD1w', 'l62WO2kz3lmJifV7VkGZY4SAcWzkjtUw3Unj7zjE')
+settings  = pd.read_csv('Settings.csv',error_bad_lines = False)
 
 #get the data files from the directory
 dataFiles = os.listdir(dataFolder)
@@ -36,7 +29,6 @@ dataFiles = os.listdir('Data')
 df = {}
 for file in dataFiles:
     df[file] = pd.read_csv(dataFolder+'/'+file, error_bad_lines = False)
-    #df[file].index = df[file]['Id']
     
 #append loaded dataframes
 df_all = df[dataFiles[0]]
@@ -45,33 +37,16 @@ for file in dataFiles:
     if first:
         first=False
     else:
-        df_all.append(df[file])
+        df_all = df_all.append(df[file], ignore_index=True)
 
 df=df_all
 
-#pulls data from goodreads website
-genres = []
-i=0
-for ID in df['Id']:
-    try:
-        book = gc.book(ID)
-        if book.popular_shelves:
-            genres.append(book.popular_shelves)
-        if i%10 == 0:
-            print(i/10)
-            print(genres)
-            break
-        i+=1
-        time.sleep(1)
-    except:
-        print("error in ID: " + str(ID))
-        break
-
-#adds data from list to dataframe
-df['Genres'] = np.asarray(genres)
+for genre in settings['Genres']:
+    df[genre]=False
+df['Classified'] = False
 
 #remove unncessary columns
-df.drop(['Publisher'], axis=1)
+#df = df.drop(['Publisher'], axis=1)
 
 #fixing rating counts and converting to usable data
 df['RatingDist1'] = df['RatingDist1'].str[2:].astype(float,errors = 'ignore')
@@ -89,44 +64,12 @@ df['Authors'] = pd.Categorical(df['Authors'])
 #pull additional data from Goodreads
 
 print(df.head())
-
+#print(df.keys())
 #print(df.dtypes)
 
+df.to_csv('Compiled_Data.csv', index=False)
 #dataset = tf.data.Dataset.from_tensor_slices(df.values)
-
-
-
 
 #processing time overall
 print("runtime: " + str(time.time()-t0) + " seconds")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
---
+++ Final_Data_PreProcessing_Scraping.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+ECE 5831
+Final
+Nathaniel Evenhouse
+"""
+#required packages
+import time
+import pandas as pd
+from goodreads import client
+
+restart = False
+write = True
+classify_count = 1000
+backup_cycle = 250
+
+#current time, used to track run time.
+t0 = time.time()
+
+#initialize client for goodreads
+gc = client.GoodreadsClient('fm1h6CpTTcPHEgwUAvOD1w', 'l62WO2kz3lmJifV7VkGZY4SAcWzkjtUw3Unj7zjE')
+gc.authenticate('fm1h6CpTTcPHEgwUAvOD1w', 'l62WO2kz3lmJifV7VkGZY4SAcWzkjtUw3Unj7zjE')
+
+#load in datafiles
+df = pd.read_csv('Compiled_Data.csv', low_memory = False)
+settings  = pd.read_csv('Settings.csv',error_bad_lines = False, low_memory = False)
+
+if restart:
+    for genre in settings['Genres']:
+        df[genre]=False
+    df['Classified'] = False
+    
+#pulls data from goodreads website
+i=0
+currently_classified = 0
+newly_classified = 0
+t_start = time.time()
+for ID in df['Id']:
+    try:
+        if df.get_value(i,'Classified')==False:
+            book = gc.book(ID)
+            genres_of_book = book.popular_shelves
+            print("Read time for book " + str(i) + " : " + str(time.time()-t_start) + " seconds")
+            read_book_time = time.time()
+            
+            if genres_of_book:
+                for genre in settings['Genres']:
+                    is_target_in_list = False
+                    for item in genres_of_book:
+                        is_target_in_list = is_target_in_list or (genre.lower() == str(item).lower())
+                        df.at[i, genre] = is_target_in_list
+                df.at[i, 'Classified'] = True
+                print("Classification time of book " + str(i) + " : " + str(time.time()-read_book_time) + " seconds")
+                
+            if newly_classified%backup_cycle == 0 and newly_classified!=0:
+                if write:
+                    print('Backing up progress...')
+                    t_back_0 = time.time()
+                    df.to_csv('Compiled_Data.csv', index=False)
+                    print('Backup Complete, time: ' + str(time.time() - t_back_0))
+            if newly_classified>classify_count:
+                break
+            newly_classified+=1
+            if (time.time()-t_start)<1:
+                time.sleep(1-(time.time()-t_start))
+        else:
+            currently_classified+=1
+    except:
+        print("error in ID: " + str(ID))
+    i+=1
+    if newly_classified>0:
+        print("Looptime: " + str(time.time() - t_start) + "seconds")
+    t_start = time.time()
+
+if write:
+    df.to_csv('Compiled_Data.csv', index=False)
+
+#processing time overall
+print("Classified books: " + str(i))
+print("total runtime: " + str(time.time()-t0) + " seconds")
+
